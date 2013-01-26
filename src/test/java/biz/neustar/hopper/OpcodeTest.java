fc1d603adb37066d0a7dbde6ec0c88d43aee0ec0
jdamick <jeffreydamick@gmail.com>
2013-01-26 13:45:35 -0500

    removed to rewrite

--- src/test/java/biz/neustar/hopper/OpcodeTest.java
+++ src/test/java/biz/neustar/hopper/OpcodeTest.java
@@ -1,91 +1,59 @@
-// -*- Java -*-
-//
-// Copyright (c) 2005, Matthew J. Rutherford <rutherfo@cs.colorado.edu>
-// Copyright (c) 2005, University of Colorado at Boulder
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-// 
-// * Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-// 
-// * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the distribution.
-// 
-// * Neither the name of the University of Colorado at Boulder nor the
-//   names of its contributors may be used to endorse or promote
-//   products derived from this software without specific prior written
-//   permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
 package biz.neustar.hopper;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 import biz.neustar.hopper.message.Opcode;
 
-public class OpcodeTest extends TestCase {
-    public void test_string() {
+public class OpcodeTest {
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testInvalidValue() {
+        // (max is 0xF)
+        Opcode.valueOf(0x10);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testInvalidValue2() {
+        Opcode.valueOf(-1);
+    }
+    
+    
+    @Test
+    public void testValidStrings() {
         // a regular one
         assertEquals("IQUERY", Opcode.IQUERY.getName());
 
         // one that doesn't exist
         assertTrue(Opcode.valueOf(6).getName().startsWith("RESERVED"));
-
-        try {
-            Opcode.valueOf(-1);
-            fail("IllegalArgumentException not thrown");
-        } catch (IllegalArgumentException e) {
-        }
-
-        // (max is 0xF)
-        try {
-            Opcode.valueOf(0x10);
-            fail("IllegalArgumentException not thrown");
-        } catch (IllegalArgumentException e) {
-        }
     }
 
-    public void test_value() {
-        // regular one
+    @Test
+    public void testValidValueOf() {
         assertEquals(Opcode.STATUS, Opcode.valueOf("STATUS"));
-
-        // one thats undefined but within range
+    }
+    
+    @Test
+    public void testReservedValue() {
         assertEquals(6, Opcode.valueOf("RESERVED6").getValue());
-        
-        // one thats undefined but out of range
-        try {
-            assertEquals(-1, Opcode.valueOf("RESERVED" + 0x10).getValue());
-            fail("IllegalArgumentException not thrown");
-        } catch (IllegalArgumentException e) {
-        }
-
-        try {
-            // something that is unknown
-            assertEquals(-1, Opcode.valueOf("THIS IS DEFINITELY UNKNOWN").getValue());
-            fail("IllegalArgumentException not thrown");
-        } catch (IllegalArgumentException e) {
-        }
-
-        try {
-            // empty string
-            assertEquals(-1, Opcode.valueOf("").getValue());
-            fail("IllegalArgumentException not thrown");
-        } catch (IllegalArgumentException e) {
-        }
-
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testInvalidReservedValue() {
+        assertEquals(-1, Opcode.valueOf("RESERVED" + 0x10).getValue());
+    }
+    
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testUnknownValueOf() {
+        assertEquals(-1, Opcode.valueOf("SOMETHING THAT IS UNKNOWN").getValue());
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testEmptyValueOf() {
+        assertEquals(-1, Opcode.valueOf("").getValue());
     }
 }
+
